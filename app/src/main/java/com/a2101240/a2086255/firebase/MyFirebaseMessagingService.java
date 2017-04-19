0e4d084d1e38e81a92011ac46e07b0e679570c41
jean9108 <joana9108@gmail.com>
2017-04-19 10:05:28 -0500

    segunda parte mal

--- app/src/main/java/com/a2101240/a2086255/firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/a2101240/a2086255/firebase/MyFirebaseMessagingService.java
@@ -1,6 +1,14 @@
 package com.a2101240.a2086255.firebase;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 /**
  * Created by 2086255 on 4/19/17.
@@ -10,4 +18,17 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "MyFirebaseMsgService";
 
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle()
+                        .setContentText();
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        
+    }
 }
